b9d3bb313110107aff13b18a15ba25c5ba3770a0
justinpheng <justinphengmyrath@gmail.com>
2023-04-11 12:56:15 -0700

    animation code

---
+++ js/script2.js
@@ -0,0 +1,8 @@
+const sr = ScrollReveal ({
+  distance: '40px',
+  duration: 2500,
+  reset: true
+});
+
+sr.reveal('.menu-btn',{delay:50, origin: 'left'});
+sr.reveal('.icons',{delay:100, origin: 'bottom'});
