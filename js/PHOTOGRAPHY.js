d2b8579f7c6aa04e85e2b67c973ce2311bd7e62e
justinpheng <justinphengmyrath@gmail.com>
2023-04-11 14:24:13 -0700

    update too

---
+++ js/PHOTOGRAPHY.js
@@ -0,0 +1,8 @@
+const sr = ScrollReveal ({
+  distance: '40px',
+  duration: 2500,
+  reset: true
+});
+
+sr.reveal('.buttons',{delay:400, origin: 'right'});
+sr.reveal('.title',{delay:200, origin: 'top'});
---
+++ js/index.js
@@ -0,0 +1,14 @@
+const sr = ScrollReveal ({
+  distance: '40px',
+  duration: 2500,
+  reset: true
+});
+
+sr.reveal('.menu-btn',{delay:200, origin: 'left'});
+sr.reveal('.icons',{delay:400, origin: 'bottom'});
+sr.reveal('.content h2',{delay:520, origin: 'top'});
+sr.reveal('.content h3',{delay:600, origin: 'top'});
+sr.reveal('.content p',{delay:680, origin: 'left'});
+sr.reveal('.btns',{delay:750, origin: 'right'});
+
+sr.reveal('.buttons',{delay:10, origin: 'left'});
